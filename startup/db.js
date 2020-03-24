50bc4b5824880eb78bdddbdcd38674bf8376788f
kopianan <ananalfredcarlos@gmail.com>
2020-03-24 23:01:27 +0700

    change mongodb connection string

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,11 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 
 module.exports = function () {
-    mongoose.connect('mongodb://localhost/Iruka')
+    mongoose.connect('mongodb+srv://kopianan:ananalfred@iruka-db-12lvj.gcp.mongodb.net/test?retryWrites=true&w=majority')
         .then(() => winston.info("Connected to mongodb"));
 
 }
+// mongodb+srv://kopianan:15121996%40aacH@iruka-db-12lvj.gcp.mongodb.net/test?retryWrites=true&w=majority
+// mongodb+srv://kopianan:15121996%40aacH@iruka-db-12lvj.gcp.mongodb.net/test
+//compas 
+// mongodb+srv://kopianan:15121996%40aacH@iruka-db-12lvj.gcp.mongodb.net/test
